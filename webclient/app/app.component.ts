30f8f1574bc65bb21047ca348f473fcca003eede
Susumu Fukuda <fukudas@jp.ibm.com>
2016-07-29 00:39:27 +0900

    #2 add base ng2 content
    - build stuffs
    - just shows the title

---
+++ webclient/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fmdash-app',
+  template: '<h1>Angular2 Fleet Management Dashboard App</h1>'
+})
+export class AppComponent { }
