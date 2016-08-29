1867ba00555f4a188ee53709534e89b8f1267707
Daisuke Maruyama <marudai@jp.ibm.com>
2016-08-29 16:54:40 +0900

    Close #10: Alert page cannot be shown on Safari
    Change variable name
    https://github.ibm.com/Watson-IoT/iota-starter-server-fleetmanagement/issues/issue/10

--- webclient/app/utils/moment.pipe.ts
+++ webclient/app/utils/moment.pipe.ts
@@ -4,7 +4,12 @@ import * as moment from 'moment';
 @Pipe({name: 'moment'})
 export class MomentPipe implements PipeTransform{
 	transform(date:any, format:string){
+<<<<<<< Upstream, based on branch 'master' of https://github.ibm.com/Watson-IoT/iota-starter-server-fleetmanagement
 		var now = date ? moment(date) : moment();
 		return format ? now.format(format) : now.format();
+=======
+		var _date = date ? moment(date) : moment();
+		return format ? _date.format(format) : _date.format();
+>>>>>>> 97dde84 Close #10: Alert page cannot be shown on Safari
 	}
 }
